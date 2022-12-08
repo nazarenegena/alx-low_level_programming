43dbed288aab33c98ebaa7063dd9191cb74b5f36
nazarene <genarene96@gmail.com>
2022-12-08 08:10:56 -0800

    push4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 
 /**
- **main - Entry point
- **Return: Always 0 (Success)*/
+ *main-entry point
+ *
+ * return always 0
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
