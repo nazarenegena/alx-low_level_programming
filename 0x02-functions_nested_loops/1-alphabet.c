486efdb32736c953e681e4a075bffd6288741a3f
nazarene <genarene96@gmail.com>
2022-12-13 06:17:15 -0800

    1aplhabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints alphabet
+ *
+ * Return :Always 0 (sucess)
+ *
+ */
+void print_alphabet(void)
+{
+char i;
+for (i = 'a'; i <= 'z'; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
