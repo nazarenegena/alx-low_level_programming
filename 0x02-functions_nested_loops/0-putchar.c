f03ff491d8519266a0c566230885c3f56a07a45b
nazarene <genarene96@gmail.com>
2022-12-13 06:00:09 -0800

    0 putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#includes "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: programs description
+ *
+ * Return: Always returns 0 (Sucess)
+ *
+ */
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
+}
