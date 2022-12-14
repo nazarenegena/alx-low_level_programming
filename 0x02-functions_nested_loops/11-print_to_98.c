e30d11d7d7373c98fc0cfabd3d854c375efa5b80
nazarene <genarene96@gmail.com>
2022-12-14 05:41:09 -0800

    11print

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,10 +1,8 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
  * print_to_98 - prints all the integers between the number given and 98
  * @n: number to count from
- *
  * Return: void
  */
 void print_to_98(int n)
