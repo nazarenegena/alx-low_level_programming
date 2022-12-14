cd9dd201adcc931677639091add67c40a0d78016
nazarene <genarene96@gmail.com>
2022-12-13 19:37:27 -0800

    print 100 times

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * print_times_table - times table function
+ * @n: interger to return
+ *
+ */
+void print_times_table(int n)
+{
+int i;
+int j;
+if (n <= 15 && n >= 0)
+{
+for (j = 0; i <= n; j++)
+{
+int prod = j * i;
+if (j == 0)
+{
+_putchar('0');
+}
+else if (prod <= 9)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar(' ');
+_putchar('0' + prod);
+}
+else if (prod > 99)
+{
+_putchar(',');
+_putcahr(' ');
+_putchar('0' + (prod / 100));
+_putchar('0' + ((prod / 10) % 10));
+_putchar('0' + (prod % 10));
+}
+else
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar('0' + (prod / 10));
+_putchar('0' + (prod % 10));
+}
+}
+_putchar('\n');
+}
+}
+}
