7305b1c8839df638cc5f5bc3e3b40efe2fd7702b
nazarene <genarene96@gmail.com>
2023-01-17 06:01:19 -0800

    2mainc

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - print the name of the file it was compiled from,
+ * Return: 0 on success
+ */
+int main(void)
+{
+printf("%s\n", _FILE_);
+return (0);
+}
