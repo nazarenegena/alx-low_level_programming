1abb5c830e7848940e50cd045a1edd648c219e93
nazarene <genarene96@gmail.com>
2023-01-30 09:30:05 -0800

    3node

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,36 +1,60 @@
 #include "lists.h"
 /**
-* add_node_end - adds node to end of a list
-* @head: pointer to pointer to head of list
-* @str: string to copy to node
-* Return: address of new element or NULL if failed
+* _strlen - a function that returns the length of a string.
+* @str: input string
+* Return: length of string
+*/
+int _strlen(const char *str)
+{
+int contador = 0;
+while (str[contador] != '\0')
+{
+contador++;
+}
+return (contador);
+}
+/**
+* add_node_end - print len.
+* @head: list.
+* @str: string.
+* Return: The elements linked.
 */
 list_t *add_node_end(list_t **head, const char *str)
 {
-list_t *node;
-list_t *copy;
-int i;
-if (!head)
-return (NULL);
-node = malloc(sizeof(list_t));
-if (!node)
-return (NULL);
-if (str)
-node->str = strdup(str);
-node->next = NULL;
-for (i = 0; *(str + i);)
-i++;
-node->len = i;
-if (!(*head))
+list_t *newnode;
+list_t *endnode;
+newnode = malloc(sizeof(list_t));
+if (newnode == NULL)
 {
-*head = node;
-return (node);
+free(newnode);
+return (NULL);
 }
-copy = *head;
-while (copy->next)
+if (*head == NULL)
 {
-copy = copy->next;
+*head = newnode;
 }
-copy->next = node;
-return (node);
+else
+{
+endnode = *head;
+while (endnode->next != NULL)
+endnode = endnode->next;
+endnode->next = newnode;
+}
+if (str == NULL)
+{
+newnode->str = 0;
+newnode->len = 0;
+}
+else
+{
+newnode->str = strdup(str);
+if (newnode->str == 0)
+{
+free(newnode);
+return (NULL);
+}
+newnode->len = _strlen(str);
+}
+newnode->next = NULL;
+return (newnode);
 }
