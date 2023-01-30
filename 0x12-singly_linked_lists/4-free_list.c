d43d616d8a9f051264eefd5f3378e21db6303585
nazarene <genarene96@gmail.com>
2023-01-30 09:40:22 -0800

    4free

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,13 +1,14 @@
 #include "lists.h"
 /**
-* free_list - Frees a list_t list.
-* @head: A pointer to the list_t list.
+* free_list - free list.
+* @head: head.
+* Return: NA.
 */
 void free_list(list_t *head)
 {
-if (!head)
+if (head == NULL)
 return;
-if (head->next)
+if (head->next != NULL)
 free_list(head->next);
 free(head->str);
 free(head);
