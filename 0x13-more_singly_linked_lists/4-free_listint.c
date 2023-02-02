fa47b349f447f24ec044c1b5dcd3392e449ea955
nazarene <genarene96@gmail.com>
2023-02-02 03:28:09 -0800

    4free

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -2,9 +2,10 @@
 /**
  * free_listint - frees a listint_t list.
  * @head: pointer to first list element.
+ *
  * Return: void.
  */
-void free_listint(lisint_t *head)
+void free_listint(listint_t *head)
 {
 listint_t *temp;
 while (head != NULL)
